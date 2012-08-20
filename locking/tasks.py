9c0b250c55ce6dd642713223b11772c5dfc21763
Llewellyn Hinkes-Jones <lhinkesjones@theatlantic.com>
2012-08-20 15:36:54 -0400

    Adding task to delete stale locks so that posts arent permentally locked

---
+++ locking/tasks.py
@@ -0,0 +1,10 @@
+
+
+def remove_stale_locks():
+	from datetime import datetime, timedelta
+	from django.db import connection, transaction
+	cursor = connection.cursor()
+	
+	cursor.execute("DELETE FROM locking_lock WHERE locked_at < %s", [datetime.now() - timedelta(days=31)]) 
+	
+	transaction.commit_unless_managed()
